30f76147ffdc610fcf51a76cda37bae940586a5b
GamerCleanVic <jonassd2000@gmail.com>
2022-12-12 18:54:30 -0500

    +matematicaConsoleLog

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
-let n1 = 10;
-let n2 = 2;
-console.log(`SOMA: `);
-console.log(`SUBTRAÇÃO: `);
-console.log(`MULTIPLICAÇÃO: `);
+const Matematica = require('./Matematica');
+var n1 = 10;
+var n2 = 2;
+console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
+console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1, n2)}`);
+console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1, n2)}`);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-let n1: number = 10;
-let n2: number = 2;
+const Matematica = require('./Matematica');
 
-console.log(`SOMA: `);
-console.log(`SUBTRAÇÃO: `);
-console.log(`MULTIPLICAÇÃO: `);
+var n1: number = 10;
+var n2: number = 2;
+
+console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
+console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1, n2)}`);
+console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1, n2)}`);
