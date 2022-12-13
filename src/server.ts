37c424da499ed5bd0ffe97f8c5ac555bb1d54f1d
GamerCleanVic <jonassd2000@gmail.com>
2022-12-12 22:13:42 -0500

    +rotasContato

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,8 @@ const server = express();
 server.get('/', (req: Request, res: Response) => {
     res.send('Olá Mundo!');
 });
+server.get('/contato', (req: Request, res: Response) => {
+    res.send('Esta é a página de contato');
+});
 
 server.listen(3003);
