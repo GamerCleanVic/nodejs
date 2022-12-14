f82704cd12ce1c99c8f05126ac64656e2cca2db3
GamerCleanVic <jonassd2000@gmail.com>
2022-12-13 22:18:01 -0500

    +Routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
-import express, { Request, Response } from 'express';
-
+import express from 'express';
+import mainRoutes from './routes/index';
+import painelRoutes from './routes/painel';
 const server = express();
 
-server.get('/', (req: Request, res: Response) => {
-    res.send('Olá Mundo!');
-});
-server.get('/contato', (req: Request, res: Response) => {
-    res.send('Esta é a página de contato');
-});
+server.use('/', mainRoutes);
+server.use('/painel', painelRoutes);
 
 server.listen(3003);
